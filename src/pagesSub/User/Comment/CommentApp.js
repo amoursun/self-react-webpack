71688667004e96e050faa21f57e410aa662a749b
v_ganyanlin <v_ganyanlin@baidu.com>
2017-12-04 13:15:24 +0800

    user改造

--- src/pagesSub/User/Comment/CommentApp.js
+++ src/pagesSub/User/Comment/CommentApp.js
@@ -2,21 +2,12 @@ import React, {Component} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 import axios from 'axios';
-// import cjson from './Comment.json';
-// const fs = require('browserify-fs');
-
-// const comments = [
-//     {username: 'Jerry', content: '练兵备战强势推进，训战一体疾风劲吹', date: 15000020338353},
-//     {username: 'Tomy', content: '聚力打仗主责主业，上下同欲真抓实备', date: 15000020338353},
-//     {username: 'Lucy', content: '能力重塑利箭在弦，一声令下战则必胜', date: 15000020338353}
-// ];
 
 class CommentApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
             comments: []
-            // comments: comments || []
         };
         this.handleSubmitComment = this.handleSubmitComment.bind(this);
         this.handleClose = this.handleClose.bind(this);
@@ -24,10 +15,16 @@ class CommentApp extends Component {
 
     componentDidMount() {
         //读取文件/接口
-        axios.get(`https://raw.githubusercontent.com/ganyanlins/api/master/react/Comment.json`)
-            .then(res => {
+        // axios.get(`https://raw.githubusercontent.com/ganyanlins/api/master/react/Comment.json`)
+        //     .then(res => {
+        //         this.setState({
+        //             comments: res.data.data
+        //         });
+        //     });
+        axios.get(`/data/Comment.json`)
+            .then((res) => {
                 this.setState({
-                    comments: res.data.data
+                    comments: res.data
                 });
             });
     };
--- src/pagesSub/User/UserSlect.js
+++ src/pagesSub/User/UserSlect.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 function OptionsList(props) {
 
@@ -7,7 +8,7 @@ function OptionsList(props) {
             <label>
                 <select value={props.initValue} onChange={props.optionChange}>
                     {
-                        props.form.options.map((option) =>
+                        props.options.map((option) =>
                             <option value={option.optionName} key={option.id}>{option.optionName}</option>
                         )
                     }
@@ -17,68 +18,71 @@ function OptionsList(props) {
     );
 };
 
-const form = {
-    options: [
-        {id: 0, optionValue: 'Select', optionName: '请选择'},
-        {id: 1, optionValue: 'Shanghai', optionName: '上海'},
-        {id: 2, optionValue: 'Beijing', optionName: '北京'},
-        {id: 3, optionValue: 'Tianjin', optionName: '天津'},
-        {id: 4, optionValue: 'Xian', optionName: '西安'},
-        {id: 5, optionValue: 'Chongqing', optionName: '重庆'},
-        {id: 6, optionValue: 'Lanzhou', optionName: '兰州'},
-        {id: 7, optionValue: 'Nanjing', optionName: '南京'},
-        {id: 8, optionValue: 'Hangzhou', optionName: '杭州'},
-        {id: 9, optionValue: 'Xuzhou', optionName: '徐州'},
-        {id: 10, optionValue: 'Chengdu', optionName: '成都'}
-    ],
-    userName: '哈哈哈'
-
-};
-
 class UserSelect extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: form.options[0].id,
-            optionValue: form.options[0].optionValue,
-            optionName: form.options[0].optionName,
-            name: form.userName
-        };
+            options: [],
+            userName: 'ganyanlin'
+        }
+        ;
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {
+        axios.get(`/data/userSelect.json`)
+            .then((res) => {
+                let data = res.data;
+                this.setState({
+                    options: data.options,
+                    userName: data.userName
+                });
+            });
+    };
+
     handleInputChange(e) {
         this.setState({
-            name: e.target.value
+            userName: e.target.value
         });
     }
     handleSelectChange(e) {
+        this.state.options.map(value => {
+            value.optionName === e.target.value ? value.name = e.target.value : delete value.name;
+        });
         this.setState({
-            optionName: e.target.value
+            options: this.state.options
         });
     }
 
     handleSubmit(e) {
-        if (this.state.optionName !== '请选择') {
-            alert(this.state.name + ', Your favorite flavor is: ' + this.state.optionName);
+        const { options, userName } = this.state;
+        this.value = '';
+        options.map(option => {
+            option.name ? this.value = option.optionName : '';
+        });
+        if (this.value) {
+            alert(userName + ', Your favorite flavor is: ' + this.value);
         }
         else {
-            alert('Warning: ' + this.state.name + ', please select your favorite city');
+            alert('Warning: ' + userName + ', please select your favorite city');
         }
         e.preventDefault();
     }
 
     render() {
+        const { options, userName } = this.state;
+        let valueName = '';
+        options.map(option => option.name ? valueName = option.optionName : '');
         return (
             <div className="user-left">
-                <h3>UserName: {form.userName || this.props.params.name}</h3>
+                <h3>UserName: { userName }</h3>
                 <form onSubmit={this.handleSubmit}>
-                    <OptionsList form={form} optionChange={this.handleSelectChange} initValue={this.state.optionName}/>
+                    <OptionsList options={options} optionChange={this.handleSelectChange} initValue={options.length > 0 ? (valueName || options[0].optionName) : ''}/>
                     <label>
                         Name:
-                        <input type="text" value={this.state.name} onChange={this.handleInputChange} />
+                        <input type="text" value={ userName } onChange={this.handleInputChange} />
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
