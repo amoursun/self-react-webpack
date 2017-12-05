205827922a0c1a2c08278e7be1e304104c79069b
v_ganyanlin <v_ganyanlin@baidu.com>
2017-12-05 16:29:20 +0800

    分页未开发,下次继续

--- src/pages/Markdown/SubMd/generate.js
+++ src/pages/Markdown/SubMd/generate.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-function Item(props) {
-    let info = props.value;
-    return <span><img src={info.avatar} alt={info.name}/>{info.name}</span>;
-}
-
 export default class Generate extends Component {
     constructor(props) {
         super(props);
@@ -15,39 +10,30 @@ export default class Generate extends Component {
     };
 
     componentDidMount() {
-        try {
-            axios.get(`http://localhost:3000/people`) //mock下 json-server generate.js 启动
-                .then((res) => {
-                    this.setState({
-                        data: res.data
-                    });
-                });
-        }
-        catch (e) {
+            // axios.get(`http://localhost:3000/people`) //mock下 json-server generate.js 启动
+            //     .then((res) => {
+            //         this.setState({
+            //             data: res.data
+            //         });
+            //     });
             axios.get(`/data/generate.json`) //json-server generate.js 没启动
                 .then((res) => {
                     this.setState({
                         data: res.data.data
                     });
                 });
-        }
     };
 
     render() {
         const { data } = this.state;
-        console.log(data)
-        // let InfoItem = (
-        //     data.map(info => {
-        //         <span key={info.id}>
-        //             <img src={info.avatar} alt={info.name}/>
-        //             {info.name}
-        //         </span>
-        //     })
-        // );
         return (
             <div className="generate">
-                {/*{ InfoItem }*/}
-                {data.map(info => <Item key={info.id} value={info} />)}
+                { data.map(info =>
+                    <span key={info.id}>
+                        <img src={info.avatar} alt={info.name}/>
+                        {info.name}
+                    </span>
+                )}
             </div>
         )
     }
